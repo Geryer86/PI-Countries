b84d0d98b79b519303b53ec392773ebdc6a2c226
Geryer86 <german.af86@gmail.com>
2022-08-04 16:52:32 -0300

    get api

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,16 +1,24 @@
 import "./styles/LandingPage.css"
 import React from "react";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getApi } from "../actions";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getApi, getCountries } from "../actions";
 import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   const dispatch = useDispatch()
+  const allCountries = useSelector(state => state.allCountries)
+
+  const [page, setPage] = useState(0)
+  const [order, setOrder] = useState()
+  const [sort, setSort] = useState("")
+  const [continent, setCont] = useState("")
+  const [name, setName] = useState("")
 
   useEffect(() => {
+    dispatch(getCountries(sort, order, page, continent, name))
     dispatch(getApi())
-  }, [dispatch])
+  }, [dispatch, sort, order, page, continent, name])
   
   return(
     <div>
