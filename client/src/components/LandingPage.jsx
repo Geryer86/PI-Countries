3f00458d156fca6e383b91d35f7d980bb0008edd
Geryer86 <german.af86@gmail.com>
2022-08-04 16:43:59 -0300

    get api

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,8 +1,17 @@
 import "./styles/LandingPage.css"
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getApi } from "../actions";
 import { Link } from "react-router-dom";
 
 export default function LandingPage() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getApi())
+  }, [dispatch])
+  
   return(
     <div>
       <h1 className="hello">Hello World</h1>
